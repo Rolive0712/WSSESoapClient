8e95883f9fab38382d342765029ae9254f5c07f9
Rolive0712 <roshitrajan@rediffmail.com>
2014-09-01 16:05:57 -0400

    Create Default.aspx.cs

---
+++ Asp.Net4.0-Website/Default.aspx.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel.Security.Tokens;
+using System.ServiceModel.Security;
+using System.Net;
+using System.Net.Security;
+using System.Text;
+
+using Custom;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //turn off Certificate validation
+        ServicePointManager.ServerCertificateValidationCallback = (object s, X509Certificate certificate, X509Chain chain,
+                     SslPolicyErrors sslPolicyErrors) => true;
+
+        SecurityBindingElement securityElement = SecurityBindingElement.CreateUserNameOverTransportBindingElement();
+        securityElement.IncludeTimestamp = false;
+        TextMessageEncodingBindingElement encodingElement = new TextMessageEncodingBindingElement(MessageVersion.Soap11, Encoding.UTF8);
+        HttpsTransportBindingElement tranportElement = new HttpsTransportBindingElement();
+
+        CustomBinding customBinding = new CustomBinding(securityElement, encodingElement, tranportElement);
+
+        EndpointAddress address = new EndpointAddress("<site url wsdl link>?wsdl");
+        
+        cmpproxy.OrderExtWSClient orderExtWSClient = new cmpproxy.OrderExtWSClient(customBinding, address);
+        orderExtWSClient.ClientCredentials.UserName.UserName = "admin";
+        orderExtWSClient.ClientCredentials.UserName.Password = "admin";
+
+        orderExtWSClient.Endpoint.Behaviors.Add(new CustomEndpointBehavior());
+
+        var orderResult = orderExtWSClient.createOrder("1010169807", "1010169807", "48314_28698_GLOBAL", null, null);
+        var orderWithItems = orderExtWSClient.createOrderWithItems("1010169807", "1010169807", "48314_28698_GLOBAL", null, null);
+
+    }
+}
