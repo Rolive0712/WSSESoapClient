b4eb33a65e13e3c16de36e985e158a6e319f59a3
Rolive0712 <roshitrajan@rediffmail.com>
2014-09-01 16:04:05 -0400

    Create CustomEndpointBehavior.cs

---
+++ Asp.Net4.0-Website/App_Code/Custom/CustomEndpointBehavior.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Channels;
+using System.ServiceModel;
+using System.Xml;
+
+/// <summary>
+/// Summary description for CustomEndpointBehavior
+/// </summary>
+
+namespace Custom
+{
+    public class CustomEndpointBehavior : IEndpointBehavior
+    {
+        /// <summary>
+        /// Implements a modification or extension of the client across an endpoint.
+        /// </summary>
+        /// <param name="endpoint">The endpoint that is to be customized.</param>
+        /// <param name="clientRuntime">The client runtime to be customized.</param>
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            clientRuntime.MessageInspectors.Add(new ClientMessageInspector());
+        }
+
+        /// <summary>
+        /// Implement to pass data at runtime to bindings to support custom behavior.
+        /// </summary>
+        /// <param name="endpoint">The endpoint to modify.</param>
+        /// <param name="bindingParameters">The objects that binding elements require to support the behavior.</param>
+        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
+        {
+        }
+
+        /// <summary>
+        /// Implements a modification or extension of the service across an endpoint.
+        /// </summary>
+        /// <param name="endpoint">The endpoint that exposes the contract.</param>
+        /// <param name="endpointDispatcher">The endpoint dispatcher to be modified or extended.</param>
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+            
+        }
+
+        /// <summary>
+        /// Implement to confirm that the endpoint meets some intended criteria.
+        /// </summary>
+        /// <param name="endpoint">The endpoint to validate.</param>
+        public void Validate(ServiceEndpoint endpoint)
+        {
+            
+        }
+    }
+
+
+
+    /// <summary>
+    /// Summary description for ClientMessageInspector
+    /// </summary>
+    /// <summary>
+    /// Represents a message inspector object that can be added to the <c>MessageInspectors</c> collection to view or modify messages.
+    /// </summary>
+    public class ClientMessageInspector : IClientMessageInspector
+    {
+        /// <summary>
+        /// Enables inspection or modification of a message before a request message is sent to a service.
+        /// </summary>
+        /// <param name="request">The message to be sent to the service.</param>
+        /// <param name="channel">The WCF client object channel.</param>
+        /// <returns>
+        /// The object that is returned as the <paramref name="correlationState " /> argument of
+        /// the <see cref="M:System.ServiceModel.Dispatcher.IClientMessageInspector.AfterReceiveReply(System.ServiceModel.Channels.Message@,System.Object)" /> method.
+        /// This is null if no correlation state is used.The best practice is to make this a <see cref="T:System.Guid" /> to ensure that no two
+        /// <paramref name="correlationState" /> objects are the same.
+        /// </returns>
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            SoapSecurityHeader header = new SoapSecurityHeader();
+            request.Headers.Add(header);
+
+            //request.Headers.Add(MessageHeader.CreateHeader("islemKodu", "", "2"));
+            //request.Headers.Add(MessageHeader.CreateHeader("islemNedeni", "", "DÃ¶kÃ¼man iptal edildi"));
+
+            return null;
+
+        }
+
+        /// <summary>
+        /// Enables inspection or modification of a message after a reply message is received but prior to passing it back to the client application.
+        /// </summary>
+        /// <param name="reply">The message to be transformed into types and handed back to the client application.</param>
+        /// <param name="correlationState">Correlation state data.</param>
+        public void AfterReceiveReply(ref System.ServiceModel.Channels.Message reply, object correlationState)
+        {
+        }
+    }
+
+    /// <summary>
+    /// Summary description for SoapSecurityHeader
+    /// </summary>
+    public class SoapSecurityHeader : MessageHeader
+    {
+        private string _password = "admin";
+
+        private string _username = "admin";
+
+        private const string PREFIX_CP = "wsse";
+
+        public SoapSecurityHeader()
+        {
+          
+        }
+
+        public override string Name
+        {
+            //get { return "wsse:Security"; }
+            get { return "Security"; }
+        }
+
+        //public override bool MustUnderstand
+        //{
+        //    get
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        public string Id { get; set; }
+
+        public override string Namespace
+        {
+            get { return "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"; }
+        }
+
+        protected override void OnWriteStartHeader(XmlDictionaryWriter writer, MessageVersion messageVersion)
+        {
+            //base.OnWriteStartHeader(writer, messageVersion);
+            //writer.WriteXmlnsAttribute(PREFIX_CP, "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
+            //writer.WriteAttributeString("SOAP-ENV:actor", "uri:orderUser");
+
+            writer.WriteStartElement(PREFIX_CP, Name, Namespace);
+            writer.WriteXmlnsAttribute(PREFIX_CP, Namespace);
+            writer.WriteAttributeString("SOAP-ENV:actor", "uri:orderUser");
+        }
+
+        protected override void OnWriteHeaderContents(XmlDictionaryWriter writer, MessageVersion messageVersion)
+        {
+            writer.WriteStartElement(PREFIX_CP, "UsernameToken", null);
+
+            writer.WriteStartElement(PREFIX_CP, "Username", Namespace);
+            writer.WriteValue(_username);
+            writer.WriteEndElement();
+
+            writer.WriteStartElement(PREFIX_CP, "Password", Namespace);
+            writer.WriteAttributeString("Type", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText");
+            writer.WriteValue(_password);
+            writer.WriteEndElement();
+
+            writer.WriteEndElement();
+        }
+    }
+
+}
